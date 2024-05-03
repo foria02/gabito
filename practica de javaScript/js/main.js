171bca5056f22a0fbe7aac395d92e40f53b578b3
foria02 <foria0212@gmail.com>
2024-05-03 15:16:08 -0300

    mi primer comit

---
+++ practica de javaScript/js/main.js
@@ -0,0 +1,13 @@
+let nombre ="carpi";
+let apellido="colleta";
+let edad= 28;
+let pais="Argentina";
+
+let usuario1={
+    nombre:"carpi", 
+    edad:28,
+    pais:"Argentina",
+
+}
+
+console.log(usuario1["nombre"]);
